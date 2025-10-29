89ea23456452896eac3d25525f9d4b024951b77a
archiebenn <archiesnbenn@gmail.com>
2025-10-29 21:39:59 +0000

    fastq format check created

--- scripts/check_fastq.sh
+++ scripts/check_fastq.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# check_fastq.sh - a quick checking script to see if they all have 4 lines 
+# check_fastq.sh - a checking script to see if each sample fastq follows fastq format
 # each should have a label, raw sequence, + sign, and quality score line
 # this shell script must be run from the project root due to hardcoded paths for file moves and folder creation
-
 
 
 # move into samples/ (if running outside project root 'cd data/' will fail and an error message is printed)
@@ -10,9 +9,13 @@ pwd
 cd data/samples || \
 { echo "Samples directory not found, please ensure you are running this script from project root"; exit 1; }
 
+
 for fastq in *.FASTQ; do
-    lines=$(cat $fastq | wc -l)
-    if [ "$lines" -ne 4 ]; then
-        echo "File $fastq does not contain 4 lines"
-    fi
+    echo "$fastq file format:" >> fastq_format_check.txt
+    awk '{printf "%5d: %.10s\n", NR, $0}' $fastq >> fastq_format_check.txt
+    echo >> fastq_format_check.txt
 done
+
+mv fastq_format_check.txt ../../results/
+echo
+echo "FastQ check complete. Find output in results/"
--- scripts/concatenate_fastq.sh
+++ scripts/concatenate_fastq.sh
@@ -7,17 +7,17 @@
 cd data/samples || \
 { echo "Samples directory not found, please ensure you are running this script from project root"; exit 1; }
 
-fastq_folder=results/processed_FASTQ                               # set FASTQ folder name for ease
+fastq_folder=results/processed_FASTQ                                  # set FASTQ folder name for ease
 
-mkdir -p ../../$fastq_folder                                       # make processed fastq directory with folder name after moving two levels up and safe to re-run
+mkdir -p ../../$fastq_folder                                          # make processed fastq directory with folder name after moving two levels up and safe to re-run
 
-for x in {A..D}; do                                                # loops for A, B, C, D (can be changed depending on downloaded file names)
-    files=(sample${x}*)                                            # creates array called files of all files beginning with sample{letter of loop}
+for x in {A..D}; do                                                   # loops for A, B, C, D (can be changed depending on downloaded file names)
+    files=(sample${x}*)                                               # creates array called files of all files beginning with sample{letter of loop}
     
-    echo "Concatenating sample${x} files..."                       # helps user see operations
-    cat "${files[@]}" > "sample${x}_processed.FASTQ"               # concatenate full array of files to sample{letter of loop}_processed.FASTQ
-    mv sample${x}_processed.FASTQ ../../$fastq_folder              # move the concatenated file to processed fastq directory in other part of repo
+    echo "Concatenating sample${x} files..."                          # helps user see operations
+    cat "${files[@]}" > "sample${x}_processed.FASTQ"                  # concatenate full array of files to sample{letter of loop}_processed.FASTQ
+    mv sample${x}_processed.FASTQ ../../$fastq_folder                 # move the concatenated file to processed fastq directory in other part of repo
 done
 
 echo
-echo "Processing complete. Find FASTA files in $folder"            # helps user know where to find processed FASTA files
+echo "Processing complete. Find concatenated FASTQ files in $folder"  # helps user know where to find processed FASTA files
