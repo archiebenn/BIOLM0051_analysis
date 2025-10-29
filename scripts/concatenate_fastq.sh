1430aae57168840d24f1f5c3719cb49f10e2d948
archiebenn <archiesnbenn@gmail.com>
2025-10-29 16:25:57 +0000

    working on concatenate sh

--- scripts/concatenate_fastq.sh
+++ scripts/concatenate_fastq.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+# concatenate_fastq.sh - this script will concatenate FASTQ sample parts into one whole FASTQ for each of the samples given
 
+cd data/samples 
+ls *.FASTQ
+for i in ls *.FASTQ; do
+    echo "File 1: $i"
+done
