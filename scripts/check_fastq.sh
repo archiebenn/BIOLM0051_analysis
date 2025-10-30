7d309faa1c7dbfaa7dbcb6df645e73d268598386
archiebenn <archiesnbenn@gmail.com>
2025-10-30 11:56:49 +0000

    another small change. first 2 scripts complete

--- scripts/check_fastq.sh
+++ scripts/check_fastq.sh
@@ -3,17 +3,20 @@
 # this shell script must be run from the project root due to hardcoded paths for file moves and folder creation
 
 # move into samples/ (if running outside project root 'cd data/' will fail and an error message is printed)
-pwd
 cd data/samples || \
 { echo "Samples directory not found, please ensure you are running this script from project root"; exit 1; }
 
+# for loop to catch every FASTQ file
+for fastq in *.FASTQ; do  
 
-for fastq in *.FASTQ; do                                                             
-    echo "$fastq file format:" >> fastq_format_check.txt           # for readability
+    # identifier for readability in text file
+    echo "$fastq file format:" >> fastq_format_check.txt         
 
-    # cut out first 15 characters of every line, along with line numbering and -ba to catch all lines in file:
-    cut -c-15 "$fastq" | nl -ba  >> fastq_format_check.txt         
-    echo >> fastq_format_check.txt                                 # space for readability   
+    # cut out first 15 characters of every line, along with line numbering and -ba to catch all lines in the FASTQ file:
+    cut -c-15 "$fastq" | nl -ba  >> fastq_format_check.txt 
+
+    # space for readability         
+    echo >> fastq_format_check.txt                                   
 done
 
 # move file to results folder:
