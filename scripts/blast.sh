81c39218e80ceaba1430873858cf1b2a239779ba
archiebenn <archiesnbenn@gmail.com>
2025-11-01 23:29:59 +0000

    run

--- scripts/blast.sh
+++ scripts/blast.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # move into results/ (if running outside project root 'cd data/' will fail and an error message is printed)
 cd results || \
 { echo "Samples directory not found, please ensure you are running this script from project root"; exit 1; }
@@ -10,8 +9,10 @@ mkdir -p blast_output
 
 for fasta in FASTA_processed/*.fasta; do
 
+    # extract file base name
     name=$(basename "$fasta" _q20.fasta)
 
+    echo
     echo "Currently BLAST searching "$fasta"..."
 
     # run blastn nucleotide search remotely and save hits as a .tsv file (can be adapted for running on hpc with local db)
@@ -22,6 +23,7 @@ for fasta in FASTA_processed/*.fasta; do
         echo "=== BLAST log for "$name"_blast.tsv ==="
         date
         blastn -version
+        
         echo "Command used:"
         echo "blastn -query "$fasta" -db nt -out "$name"_blast.tsv -outfmt 6 -remote"
         
