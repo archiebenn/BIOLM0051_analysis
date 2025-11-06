a0d79141e44fb10e39fa5935441e357cb7f2ae7a
archiebenn <archiesnbenn@gmail.com>
2025-11-06 18:42:34 +0000

    testing taxonomy counts

--- scripts/5_blast_taxonomy.sh
+++ scripts/5_blast_taxonomy.sh
@@ -16,9 +16,13 @@ for tsv in 4_blast_outputs/*.tsv; do
     # run taxonkit lineage based on the staxids generated from blast script (in blast tsvs)
     cut -f3 4_blast_outputs/"$name"_Q20.fasta_blast.tsv | taxonkit lineage > "$name"_blast_lineage.tsv
 
-    # append this lineage tsv to the original blast tsv
+    # create a sorted/counted file for each of the species detected from blast search and taxonkit
+    cut -f2 "$name"_blast_lineage.tsv | sort | uniq -c | sort -nr > "$name"_taxonomies.txt
 
     # move to results folder
-    mv "$name"_blast_lineage.tsv 5_blast_taxonomies
+    mv "$name"_blast_lineage.tsv 5_blast_taxonomies 
+    mv "$name"_taxonomies.txt 5_blast_taxonomies
 
 done
+
+cd ..
