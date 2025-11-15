d7844b0db5645b286584dbae75019a737eeda068
archiebenn <archiesnbenn@gmail.com>
2025-11-15 18:03:13 +0000

    updating taxonomy count sript

--- scripts/5_blast_taxonomy.sh
+++ scripts/5_blast_taxonomy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# blast_taxonomy.sh - checks taxonomic results from blast searches
+# blast_taxonomy.sh - checks and counts taxonomic results from blast searches
 
 # move into results/ (if running outside project root 'cd data/' will fail and an error message is printed)
 cd results || \
 { echo "Samples directory not found, please ensure you are running this script from project root"; exit 1; }
 
-mkdir -p 5_blast_taxonomies
+mkdir -p 5_blast_taxonomy
 pwd
 
 for tsv in 4_blast_outputs/*.tsv; do
@@ -14,14 +14,17 @@ for tsv in 4_blast_outputs/*.tsv; do
     name=$(basename "$tsv" _Q20.fasta_blast.tsv)
 
     # run taxonkit lineage based on the staxids generated from blast script (in blast tsvs)
-    cut -f3 4_blast_outputs/"$name"_Q20.fasta_blast.tsv | taxonkit lineage > "$name"_blast_lineage.tsv
+    cut -f3 4_blast_outputs/"$name"_Q20.fasta_blast.tsv | taxonkit lineage > temp.tsv
 
     # create a sorted/counted file for each of the species detected from blast search and taxonkit
-    cut -f2 "$name"_blast_lineage.tsv | sort | uniq -c | sort -nr > "$name"_taxonomies.txt
+    cut -f1,2 temp.tsv | sort | uniq -c | sort -nr > "$name"_taxonomic_counts.txt
 
     # move to results folder
-    mv "$name"_blast_lineage.tsv 5_blast_taxonomies 
-    mv "$name"_taxonomies.txt 5_blast_taxonomies
+    mv "$name"_taxonomic_counts.txt 5_blast_taxonomy
+
+    # remove as not needed
+    rm temp.tsv 
+    
 
 done
 
