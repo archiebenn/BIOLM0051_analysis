e4ff4ff8f82aef201a79fbee3928ab70379c35ae
archiebenn <archiesnbenn@gmail.com>
2025-11-16 14:36:49 +0000

    finished script 5

--- scripts/5_blast_filtering.sh
+++ scripts/5_blast_filtering.sh
@@ -21,20 +21,20 @@ for tsv in 4_blast_outputs/*.tsv; do
     # uses awk to take the query sequence name (in column 1/$1 of tsv) and rename a file to that name, output to sample subfolder
     awk -F'\t' -v out="5_blast_filtering/$name" '{print > (out "/" $1 "_blast.tsv")}' 4_blast_outputs/"$name"_Q20.fasta_blast.tsv
 
-    # for each part of each sample, sort the top blast hit for each staxid, in order to have a diverse set of accessions across taxa
+    # for each part of each sample, sort the top blast hit for each staxid, in order to have a diverse set of accessions across taxa, and run taxonkit 
     for part in 5_blast_filtering/"$name"/*_blast.tsv; do
 
         # extract part base name
         part_name=$(basename "$part" _blast.tsv)
 
-        # awk to print line to output tsv if it hasn't 'seen' that staxid (in column 3/$3) before to ensure no repeated staxids (to not bloat during alignment)
-        awk -F'\t' '!seen[$3]++' 5_blast_filtering/"$name"/"$part_name"_blast.tsv > 5_blast_filtering/"$name"/"$part_name"_unique_taxa.tsv
-
-        # run taxonkit lineage based on the staxids generated from blast script (in blast tsvs). this is for the 
-        cut -f3 5_blast_filtering/"$name"/"$part_name"_unique_taxa.tsv | taxonkit lineage > temp.tsv
+        # run taxonkit lineage based on the staxids generated from full 'part blast tsv' for an overview of taxa hits per sample part
+        cut -f3 5_blast_filtering/"$name"/"$part_name"_blast.tsv | taxonkit lineage > temp.tsv
 
         # create a sorted/counted file for each of the species detected from blast search and taxonkit, this is just for a guide of the top hits from blast
-        cut -f1,2 temp.tsv | sort | uniq -c | sort -nr > "$part_name"_taxonomic_counts.txt
+        cut -f1,2 temp.tsv | sort | uniq -c | sort -nr > 5_blast_filtering/"$name"/"$part_name"_taxonomic_counts.txt
+
+        # awk to print line to output tsv if it hasn't 'seen' that staxid (in column 3/$3) before to ensure no repeated staxids (to not bloat during alignment)
+        awk -F'\t' '!seen[$3]++' 5_blast_filtering/"$name"/"$part_name"_blast.tsv > 5_blast_filtering/"$name"/"$part_name"_unique_taxa.tsv
 
         # remove as not needed
         rm temp.tsv 
