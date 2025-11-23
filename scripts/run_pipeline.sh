6508b91c3a847ef800b67771b5512ac217f6416d
archiebenn <archiesnbenn@gmail.com>
2025-11-23 14:05:47 +0000

    update run_pipeline.sh to allow user to choose which script to start from

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -7,8 +7,10 @@ start=$(date +%s)
 # strict mode - exit on errors and pipeline failures (also in every .sh script)
 set -eo pipefail
 
-# create hidden make folder
-mkdir -p .make
+# allow user to call step to start analysis from by adding an argument (no argument defaults to run full pipeline)
+# 1 <= $1/argument <= final script number
+# e.g to run all scripts after blast (script 4): ./scripts/run_pipeline.sh 5
+start_pipeline_from=${1:-1}
 
 cat << "EOF"
    __  _____  _____________________  __    __  __________ ______    ___   _  _____   ____  ______________
@@ -21,82 +23,114 @@ EOF
 # 0. ensure all scripts are executable
 chmod +x scripts/*
 
+
 # 1. check fastq format of samples
-echo "[1] FASTQ check beginning..."
-sleep 3
-./scripts/1_check_fastq.sh
-echo "[1] FASTQ check complete"
-echo
+if (( start_pipeline_from <= 1 )); then
+   echo "[1] FASTQ check beginning..."
+   sleep 3
+   ./scripts/1_check_fastq.sh
+   echo "[1] FASTQ check complete"
+   echo
+fi
+
 
 # 2. clean single-read fastq samples and concatenate into respective multi-read fastq files
-echo "[2] FASTQ concatenation beginning..."
-sleep 3
-./scripts/2_concatenate_fastq.sh
-echo "[2] FASTQ concatenation complete,"
-echo
+if (( start_pipeline_from <= 2 )); then
+   echo "[2] FASTQ concatenation beginning..."
+   sleep 3
+   ./scripts/2_concatenate_fastq.sh
+   echo "[2] FASTQ concatenation complete,"
+   echo
+fi
+
 
 # 3. quality control/convert multi-read fastq files to single fasta format using seqtk 
-echo "[3] FastQC and FASTQ->FASTA beginning..."
-sleep 3
-./scripts/3_fastq_to_fasta.sh
-echo "[3] FASTQ->FASTA complete. FastQC html reports can be found in results/3_FASTQC_reports"
-echo
+if (( start_pipeline_from <= 3 )); then
+   echo "[3] FastQC and FASTQ->FASTA beginning..."
+   sleep 3
+   ./scripts/3_fastq_to_fasta.sh
+   echo "[3] FASTQ->FASTA complete. FastQC html reports can be found in results/3_FASTQC_reports"
+   echo
+fi
+
 
 # 4. blast searching
-echo "[4] BLAST search beginning..."
-sleep 3
-./scripts/4_blast.sh 
-echo "[4] BLAST searches complete."
-echo
+if (( start_pipeline_from <= 4 )); then
+   echo "[4] BLAST search beginning..."
+   sleep 3
+   ./scripts/4_blast.sh 
+   echo "[4] BLAST searches complete."
+   echo
+fi
+
 
 # 5. BLAST filtering
-echo "[5] Filtering BLAST hits..."
-sleep 3
-./scripts/5_blast_filtering.sh
-echo "[5] BLAST hits filtered"
-echo
+if (( start_pipeline_from <= 5 )); then
+   echo "[5] Filtering BLAST hits..."
+   sleep 3
+   ./scripts/5_blast_filtering.sh
+   echo "[5] BLAST hits filtered"
+   echo
+fi
+
 
 # 6. manual blast selection
-echo "[6] Applying manual filters to select BLAST hits for downstream analysis..."
-sleep 3
-./scripts/6_manual_selection.sh
-echo "[6] BLAST hits selected and saved."
+if (( start_pipeline_from <= 6 )); then
+   echo "[6] Applying manual filters to select BLAST hits for downstream analysis..."
+   sleep 3
+   ./scripts/6_manual_selection.sh
+   echo "[6] BLAST hits selected and saved."
+fi
+
 
 # 7. trimmed fasta files from top, unique accessions in each part's blast output (trimmed to query sstart - send)
-echo "[7] Retrieving and trimming selected FASTA files with efetch..."
-sleep 3
-./scripts/7_efetch.sh
-echo "[7] efetch FASTA files retrieved and trimmed to match query."
-echo
+if (( start_pipeline_from <= 7 )); then
+   echo "[7] Retrieving and trimming selected FASTA files with efetch..."
+   sleep 3
+   ./scripts/7_efetch.sh
+   echo "[7] efetch FASTA files retrieved and trimmed to match query."
+   echo
+fi
+
 
 # 8. combine the original sample part to its respctive trimmed fasta file
-echo "[8] Concatenating query FASTAs with BLAST FASTAs..."
-sleep 3
-./scripts/8_concatenate_fasta.sh
-echo "[8] FASTA concatenation complete. Find complete FASTA files in results/7_complete_FASTA"
-echo
+if (( start_pipeline_from <= 8 )); then
+   echo "[8] Concatenating query FASTAs with BLAST FASTAs..."
+   sleep 3
+   ./scripts/8_concatenate_fasta.sh
+   echo "[8] FASTA concatenation complete. Find complete FASTA files in results/7_complete_FASTA"
+   echo
+fi
+
 
 # 9. biopython to translate full fasta files and select appropriate frame for protein sequence
-echo "[9] Using Biopython to translate nt FASTA sequences and select correct frame..."
-sleep 3
-python3 scripts/9_translation.py
-echo "[9] Biopython translation and frame selection complete. Find protein sequences in results/8_protein_FASTA"
-echo
+if (( start_pipeline_from <= 9 )); then
+   echo "[9] Using Biopython to translate nt FASTA sequences and select correct frame..."
+   sleep 3
+   python3 scripts/9_translation.py
+   echo "[9] Biopython translation and frame selection complete. Find protein sequences in results/8_protein_FASTA"
+   echo
+fi
 
 
 # 10. alignment use muscle5
-echo "[10] Aligning protein sequences with MUSCLE..."
-sleep 3
-./scripts/10_muscle_alignment.sh
-echo "[10] MUSCLE protein alignment complete. Find alignment files in results/9_alignment_files"
-echo
+if (( start_pipeline_from <= 10 )); then
+   echo "[10] Aligning protein sequences with MUSCLE..."
+   sleep 3
+   ./scripts/10_muscle_alignment.sh
+   echo "[10] MUSCLE protein alignment complete. Find alignment files in results/9_alignment_files"
+   echo
+fi
+
 
 # 11. tree build
-echo "[11] Building phylogenetic trees with IQ-TREE..."
-sleep 3
-./scripts/11_build_tree.sh
-echo "[11]Phylogenetic tree builds complete. Find tree files in results/10_tree_files"
-echo
+if (( start_pipeline_from <= 11 )); then
+   echo "[11] Building phylogenetic trees with IQ-TREE..."
+   sleep 3
+   ./scripts/11_build_tree.sh
+   echo "[11]Phylogenetic tree builds complete. Find tree files in results/10_tree_files"
+   echo
+fi
 
 end=$(date +%s)
 runtime=$((end - start))
